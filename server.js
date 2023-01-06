d9e790b19c5ff36dea794270d66c2e95ea018f05
killshot11 <theamikarsinha@gmail.com>
2023-01-06 15:39:19 +0530

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json());
+app.use(express.json({extended : false}));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -19,9 +19,9 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
